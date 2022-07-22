9b58b576067cfdc284a9c1a13bcc786d2d433c03
Abhayraj <abyraj1999@gmail.com>
2022-07-23 00:14:20 +0530

    heroku deployment changes

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 import unittest
-from app import create_app, db
+from app import app, db
 from app.models import User, Post
 from config import Config
 
@@ -12,7 +12,7 @@ class TestConfig(Config):
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
-        self.app = create_app(TestConfig)
+        self.app = app(TestConfig)
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
